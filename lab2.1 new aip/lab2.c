d7d837cc332b603107a328791b620529088d35d0
Romania322 <31658790+Romania322@users.noreply.github.com>
2017-11-02 08:07:58 +0300

    code style

--- lab2.1 new aip/lab2.c
+++ lab2.1 new aip/lab2.c
@@ -1,37 +1,47 @@
+/*
+
+*/
+
 #define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h> 
 #include <conio.h> 
+
+#define ARRAY_SIZE 100
+
 int function_enter(void); 
+
 int main() 
 { 
-int sum = 0, size, count2, count1, check, cheksize; 
-printf("Enter nomber of the size:"); 
-cheksize = function_enter(); 
-int array[100]; 
+  int sum = 0, size, check, chekSize; 
+  printf("Enter nomber of the size:"); 
+  cheksize = function_enter(); 
+  int array[ARRAY_SIZE]; 
 
-for (count1 = 0; count1 < cheksize; count1++) { 
-printf("Enter element:"); 
-array[count1] = function_enter(); 
-} 
-for (count2 = 0; count2 < cheksize; count2++) 
-{ 
-if (array[count2]>0) 
-sum = sum + array[count2]; 
-} 
+  for (int i = 0; i < cheksize; i++) { 
+    printf("Enter element:"); 
+    array[i] = function_enter(); 
+  } 
+  for (int i = 0; i < cheksize; i++) 
+  { 
+    if (array[i] > 0) {
+      sum += array[i]; 
+    }
+  } 
 
-printf("%d", sum); 
-_getch(); 
-return 0; 
-
+  printf("%d", sum); 
+  _getch(); 
+  return 0; 
 } 
 
 int function_enter(void) { 
-int number; 
-scanf("%d", &number); 
-if (getchar() != '\n') { 
-printf("Error number. Try again:"); 
-while (getchar() != '\n'); 
-number = function_enter(); 
-} 
-return number; 
+  int number; 
+  scanf("%d", &number); 
+  
+  if (getchar() != '\n') { 
+    printf("Error number. Try again:"); 
+    while (getchar() != '\n'); 
+    number = function_enter(); 
+  } 
+  
+  return number; 
 }
