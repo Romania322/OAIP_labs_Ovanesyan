eccc99140ffeb7b6981320fff04717bdc3442ebe
Romania322 <31658790+Romania322@users.noreply.github.com>
2017-11-05 20:05:43 +0300

    Сделал подсчет суммы в функции

--- lab2.1 new aip/lab2.c
+++ lab2.1 new aip/lab2.c
@@ -1,7 +1,7 @@
 /*
-  Ованесян Даниил Арменович КТБО 1-8
-  Лабораторная №2 вариант 4
-  Найти сумму всех положительных элементов массива
+Ованесян Даниил Арменович КТБО 1-8
+Лабораторная №2 вариант 4
+Найти сумму всех положительных элементов массива
 */
 
 #define _CRT_SECURE_NO_WARNINGS 
@@ -10,40 +10,57 @@
 
 #define ARRAY_SIZE 100
 
-int enterArray(void); 
+int enterArray(void);
+int enterSum(void);
 
-int main() 
-{ 
-  int sum = 0, size, userLength; 
-  printf("Enter nomber of the size:"); 
-  userLength =enterArray(); 
-  int array[ARRAY_SIZE]; 
+int mainArray[100];
+int main()
+{
+	int sum = 0, size, userLength;
+	printf("Enter nomber of the size:");
+	userLength = enterArray();
+	int array[ARRAY_SIZE];
 
-  for (int i = 0; i < userLength; i++) { 
-    printf("Enter element:"); 
-    array[i] = enterArray(); 
-  } 
-  for (int i = 0; i < userLength; i++) 
-  { 
-    if (array[i] > 0) {
-      sum += array[i]; 
-    }
-  } 
+	for (int i = 0; i < userLength; i++) {
+		printf("Enter element:");
+		mainArray[i] = enterArray();
+	}
+	size = enterSum();
 
-  printf("%d", sum); 
-  _getch(); 
-  return 0; 
-} 
+	printf("%d", size);
+	_getch();
+	return 0;
+}
 
-int enterArray(void) { 
-  int number; 
-  scanf("%d", &number); 
-  
-  if (getchar() != '\n') { 
-    printf("Error number. Try again:"); 
-    while (getchar() != '\n'); 
-    number = enterArray(); 
-  } 
-  
-  return number; 
+int enterArray(void) 
+{
+	int number;
+	scanf("%d", &number);
+
+	if (getchar() != '\n') 
+	{
+		printf("Error number. Try again:");
+		while (getchar() != '\n');
+		number = enterArray();
+	}
+
+	return number;
+}
+int enterSum(void)
+{
+
+	int sum = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		if(mainArray[i] > 0)
+		{
+			sum = sum + mainArray[i];
+		}
+		else
+		{
+			return sum;
+			break;
+		}
+	}
+
 }
