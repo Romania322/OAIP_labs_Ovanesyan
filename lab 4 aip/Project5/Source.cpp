c2edc54976f8af0056352eb11e7a639a79c854c8
Romania322 <31658790+Romania322@users.noreply.github.com>
2017-11-30 00:06:02 +0300

    1

--- lab 4 aip/Project5/Source.cpp
+++ lab 4 aip/Project5/Source.cpp
@@ -1,12 +1,12 @@
 /*
-	Ованесян Даниил 4 лаба 10 вариант
-	Сделать считывание данных с файла 
-	КТБО 1-8
-/*
+Ованесян Даниил 4 лаба 10 вариант
+Сделать считывание данных с файла
+КТБО 1-8
+*/
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h> 
-#include <conio.h> 
-#include <locale> 
+#include <stdio.h>
+#include <conio.h>
+#include <locale>
 #define MAX_LENGTH 1024
 
 int replace(int stop , char *text );
@@ -15,65 +15,65 @@ int readit(int stop, char *text, FILE *SourceFile);
 
 int main()
 {
-	system("chcp 1251");
-	system("cls");
-	FILE *File = fopen("text1.txt", "r");
-	FILE *NewFile = fopen("text2.txt", "w");
-	char text[MAX_LENGTH];
-	int stop;
-	stop = lenght(File);
-	fclose(File);
-	FILE *SourceFile = fopen("text1.txt", "r");
-	readit(stop, text, SourceFile);
-	replace(stop, text);
-	for (int i = 0; i < stop; i++)
-	{
-		fprintf(NewFile, "%c", text[i]);
-	}
-	fclose(NewFile);
-	fclose(SourceFile);
-	
-	
-	return 0;
+system("chcp 1251");
+system("cls");
+FILE *File = fopen("text1.txt", "r");
+FILE *NewFile = fopen("text2.txt", "w");
+char text[MAX_LENGTH];
+int stop;
+stop = lenght(File);
+fclose(File);
+FILE *SourceFile = fopen("text1.txt", "r");
+readit(stop, text, SourceFile);
+replace(stop, text);
+for (int i = 0; i < stop; i++)
+{
+fprintf(NewFile, "%c", text[i]);
+}
+fclose(NewFile);
+fclose(SourceFile);
+
+
+return 0;
 }
 
 int lenght(FILE *SourceFile)
 {
-	int stop = 0;
-	for (int i = 0; fgetc(SourceFile) != EOF; i++)
-	{
-		stop = stop + 1;
-		}
-	return stop;
+int stop = 0;
+for (int i = 0; fgetc(SourceFile) != EOF; i++)
+{
+stop = stop + 1;
+}
+return stop;
 }
 
 int readit(int stop, char *text, FILE *SourceFile)
 {
-	int i;
-	for (i = 0; i < stop; i++)
-	{
-		text[i] = fgetc(SourceFile);
-	}
-	return 0;
+int i;
+for (i = 0; i < stop; i++)
+{
+text[i] = fgetc(SourceFile);
+}
+return 0;
 }
 
 int replace(int stop, char *text)
 {
-	char buff;
-	int k = 0;
+char buff;
+int k = 0;
 
-	for (int i = 1; text[i] != '\0'; i++)
-	{
-		if (text[i] == ' ')
-		{
-			k = i + 1;
-		}
-		if ((text[i + 1] == ' ' || text[i + 1] == '\n' || i == stop-1 ) && text[k] != ' ')
-		{
-			buff = text[k];
-			text[k] = text[i];
-			text[i] = buff;
-		}
-	}
-	return 0;
+for (int i = 1; text[i] != '\0'; i++)
+{
+if (text[i] == ' ')
+{
+k = i + 1;
+}
+if ((text[i + 1] == ' ' || text[i + 1] == '\n' || i == stop-1 ) && text[k] != ' ')
+{
+buff = text[k];
+text[k] = text[i];
+text[i] = buff;
+}
+}
+return 0;
 }
